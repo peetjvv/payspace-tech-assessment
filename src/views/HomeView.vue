26ad51795e9f17e63ed798fd78a82861ea73f4c8
Peet Janse van Vuuren <peetjvv93@gmail.com>
2023-09-05 07:35:35 +0200

    save

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@ import { reverseString } from '../utils/strings';
 
 const strings = [
   'First string',
-  'Second, slightly longer, string ending with!',
+  'Second, slightly longer, string. This one contains punctuation thingiës!',
 ];
 </script>
 
@@ -13,69 +13,71 @@ const strings = [
     <h2>Home page</h2>
 
     <section>
-    <h3>Part 1</h3>
-    <p>Play around on this page and the books page</p>
-</section>
+      <h3>Part 1</h3>
+      <span>Play around on this page and the books page</span>
+    </section>
 
-    <h3>Part 2</h3>
-    <p>Navigate to the <RouterLink to="/books">Books</RouterLink> page</p>
+    <section>
+      <h3>Part 2</h3>
+      <span>Navigate to the <RouterLink to="/books">Books</RouterLink> page</span>
+    </section>
 
-    <br />
+    <section>
+      <h3>Part 3</h3>
+      <p>Play with the counter below</p>
+      <CounterComponent />
+    </section>
 
-    <h3>Part 3</h3>
-    <p>Play with the counter below</p>
-    <CounterComponent />
-
-    <br />
-
-    <h3>Part 4</h3>
-    <p>Reversing a string</p>
-    <div class="row">
-      <div class="col">
-        <h4>Input</h4>
-        <ul class="list-group">
-          <li
-            v-for="(str, idx) in strings"
-            v-bind:key="idx"
-            class="list-group-item"
-          >
-            {{ str }}
-          </li>
-        </ul>
+    <section>
+      <h3>Part 4</h3>
+      <span>Reversing a string</span>
+      <div class="row">
+        <div class="col">
+          <h4>Input</h4>
+          <ul class="list-group">
+            <li
+              v-for="(str, idx) in strings"
+              v-bind:key="idx"
+              class="list-group-item"
+            >
+              {{ str }}
+            </li>
+          </ul>
+        </div>
+        <div class="col">
+          <h4>Output</h4>
+          <ul class="list-group">
+            <li
+              v-for="(str, idx) in strings"
+              v-bind:key="idx"
+              class="list-group-item text-right"
+            >
+              {{ reverseString(str) }}
+            </li>
+          </ul>
+        </div>
       </div>
-      <div class="col">
-        <h4>Output</h4>
-        <ul class="list-group">
-          <li
-            v-for="(str, idx) in strings"
-            v-bind:key="idx"
-            class="list-group-item"
-          >
-            {{ reverseString(str) }}
-          </li>
-        </ul>
-      </div>
-    </div>
-    <a
-      target="_blank"
-      href="https://github.com/peetjvv/payspace-tech-assessment/blob/master/src/utils/strings.ts"
-    >
-      String reverse source code
-    </a>
+      <a
+        target="_blank"
+        href="https://github.com/peetjvv/payspace-tech-assessment/blob/master/src/utils/strings.ts"
+      >
+        See string reverse function source code
+      </a>
+    </section>
 
-    <br/>
-
-    <h3>Part 5</h3>
-    <p>Code review</p>
-    <code class="d-flex flex-column">
-      <span>1&nbsp;{{ `<ul class="item-list">` }}</span>
-      <span>2&nbsp;&nbsp;&nbsp;{{ `<% items.forEach((item) => { %>` }}</span>
-      <span>3&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ `<li><%= item.name %></li>` }}</span>
-      <span>4&nbsp;&nbsp;&nbsp;{{ `<% }); %>` }}</span>
-      <span>5&nbsp;{{ `</ul>` }}</span>
-    </code>
-    <ul>
-      <li><b>Line 2</b>: <code class="d-inline-flex">Arrays.forEach()</code> only loops over the <code class="d-inline-flex">items</code> array, but does not return any values. Use <code class="d-inline-flex">Arrays.map()</code> instead.</li>
-    </ul>
+    <section>
+      <h3>Part 5</h3>
+      <p>Code review</p>
+      <code class="d-flex flex-column">
+        <span>1&nbsp;{{ `<ul class="item-list">` }}</span>
+        <span>2&nbsp;&nbsp;&nbsp;{{ `<% items.forEach((item) => { %>` }}</span>
+        <span>3&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ `<li><%= item.name %></li>` }}</span>
+        <span>4&nbsp;&nbsp;&nbsp;{{ `<% }); %>` }}</span>
+        <span>5&nbsp;{{ `</ul>` }}</span>
+      </code>
+      <ul>
+        <li><b>Line 2</b>: <code class="d-inline-flex">Arrays.forEach()</code> only loops over the <code class="d-inline-flex">items</code> array, but does not return any values. Use <code class="d-inline-flex">Arrays.map()</code> instead.</li>
+      </ul>
+    </section>
   </main>
 </template>
